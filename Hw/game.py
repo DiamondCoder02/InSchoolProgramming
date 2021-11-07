98db81ddbf07914685d351ebb51b404bbd460f59
DiamondPRO02 <kovacsdanielmark@gmail.com>
2021-11-07 01:09:30 +0100

    FUUUUUU

--- Hw/game.py
+++ Hw/game.py
@@ -13,25 +13,18 @@ print(lang)
 print(langfile)
 print(language)
 
-# some JSON:
-z =  '{ "name":"John", "age":30, "city":"New York"}'
-# parse x:
-y = json.loads(z)
-# the result is a Python dictionary:
-print(y["age"])
-
 wins1=0
 wins2=0
-again=input('Wanna play a game?' + '\n1.Yes \n2.No \n')
+again=input(lang["wanna_play"] + lang["YN"])
 again=int(again)
 while again == 1:
-    print('Choose a gamemode:')
-    gamemode=input('1. I think of a number \n2. You think of a number \n3. Exit \n')
+    print(lang["choose_gamemode"])
+    gamemode=input(lang["menu"])
     game = int(gamemode)
     if game > 0:
         if game == 1:
             import random
-            mn=input('What is the biggest number you want to guess? \n')
+            mn=input(lang["1_big_num"])
             maxnumber= int(mn)
             num = random.randint(1, maxnumber)
             guess = None
@@ -40,37 +33,39 @@ while again == 1:
                 trynum1 = int(trynum)
                 for x in range(trynum1):
                     x1=str(x)
-                    guess = input('____________(Guesses:'+x1+') '+ 'Guess a number between 1 and ' + mn + ": ")
+                    guess = input('____________(Guesses:'+x1+') '+ lang["1_guess"] + mn + ": ")
                     guess = int(guess)
                     if guess < num:
-                        print('Low guess')
+                        print(lang["low"])
                     elif guess > num:
-                        print('Guess is high')
+                        print(lang["high"])
                     else:
                         print("\033c")
-                        print('You guessed it! Congratulations!')
+                        print(lang["won"])
                         wins1 += 1
                         break
                 else:
                     num1 = str(num)
                     print('_____________________\nYou could not guess it. I\'m sorry. The number was: ' + num1)
                     break
-        if game == 2:
+        elif game == 2:
+            print("\033c")
             print("Whut:")
             wins2 += 1
-        if game == 3:
+        elif game == 3:
             break
-        if game > 4:
-            print('Please enter a valid number.')
-        again=input('Want to play again?' + '\n1.Yes \n2.No \n')
+        else:
+            print(lang["enter_valid"])
+        again=input(lang["want_again"] + lang["YN"])
         again=int(again)
     else:
         print("\033c")
-        print('Please enter a valid number.')
+        print(lang["enter_valid"])
+
 #except:
  #   print('_____________________\nSomething went wrong. Restart the game and give correct numbers!')
 
-print('\n___***Thank you for playing***___')
+print(lang["thanks_playing"])
 win1 = str(wins1)
 win2 = str(wins2)
 print('________________You won: ' + win1 + '\n________________I, the computer won: ' + win2)
