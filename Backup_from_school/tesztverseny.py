1d364129a36a38ded01329551f854b5e48c1451c
Diamond <53056204+DiamondPRO02@users.noreply.github.com>
2021-11-18 15:25:12 +0100

    Add files via upload

--- Backup_from_school/tesztverseny.py
+++ Backup_from_school/tesztverseny.py
@@ -10,8 +10,14 @@ be_valasz=None
 for szkod,szvalasz in eredmenyek:
     if be_kod  == szkod:
         be_valasz =szvalasz
-print("\n4.feladat")
-
+        break
+print(be_valasz + " (A versenyző válasza)")
+print("\n4.feladat:")
+print("{} \t(A helyes megoldás)".format(megoldasok))
+helyes=""
+for i,jel in enumerate(megoldasok):
+    helyes+="+" if jel==be_valasz[i] else " "
+print(helyes + " \t(A versenyző helyes válaszai)")
 print("\n5.feladat")
 
 print("\n6.feladat")
