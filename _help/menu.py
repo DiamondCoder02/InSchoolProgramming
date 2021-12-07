db9bdaf6d0c4ceaf7e1667da563666ae336540c3
DiamondPRO02 <kovacsdanielmark@gmail.com>
2021-12-07 09:31:07 +0100

    no

--- _help/menu.py
+++ _help/menu.py
@@ -1,8 +1,17 @@
 import keyboard
 print("\033c")
-while True:
+while False:
     try:
         
         break
     except:
         print("\033c" + "fuck \n")
+
+keyboard.press_and_release('shift+s, space')
+
+keyboard.write('The quick brown fox jumps over the lazy dog.')
+
+keyboard.add_hotkey('ctrl+shift+a', print, args=('triggered', 'hotkey'))
+
+# Press PAGE UP then PAGE DOWN to type "foobar".
+keyboard.add_hotkey('page up, page down', lambda: keyboard.write('foobar'))
--- _help/run.bat
+++ _help/run.bat
@@ -1,5 +1,4 @@
 @echo off
 pip install keyboard
-pip install simple-term-menu
 pause
 exit
