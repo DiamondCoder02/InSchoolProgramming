565977241aa77f67bd24e2812e3b23c1c07b2c85
DiamondPRO02 <kovacsdanielmark@gmail.com>
2022-01-09 20:30:51 +0100

    Someone foce me into programming course

--- ClickClick/game.py
+++ ClickClick/game.py
@@ -1,22 +1,23 @@
 print("\033c")
 import json ; import os; import mouse; import keyboard
-click=0; time=0; actualClick=0; clickMultiplier=1
+click=0; time=0; actualClick=0; clickMultiplier=1; game=True
 
 while True:
-    try:
-        l=os.listdir('.\\languages')
-        li=[x.split('.')[0] for x in l]
-        language = str(input("Language / Nyelv: " + str(li) + "\n"))
-        langfile = '.\\languages\\'+ language + '.json'
-        with open(langfile, 'r', encoding='utf-8') as json_config:
-            lang = json.load(json_config)
-        json_config.close
-        break
-    except:
-        print("\033c" + "Write one of theses: eng, hun \nAlso don't forget tha language files. \n")
+  try:
+      l=os.listdir('.\\languages')
+      li=[x.split('.')[0] for x in l]
+      language = str(input("Language / Nyelv: " + str(li) + "\n"))
+      langfile = '.\\languages\\'+ language + '.json'
+      with open(langfile, 'r', encoding='utf-8') as json_config:
+          lang = json.load(json_config)
+      json_config.close
+      break
+  except:
+      print("\033c" + "Write one of theses: eng, hun \nAlso don't forget tha language files. \n")
         
 print(lang["welcome"])
 
+#Time to learn python classes
 class Person:
   def __init__(self, name, age):
     self.name = name
@@ -37,9 +38,3 @@ class Person:
 
 p1 = Person("John", 36)
 p1.myfunc()
-
-keyboard.wait('ctrl')
-if keyboard.read_event('alt'):
-    print("enter")
-else:
-    print("fuck")
