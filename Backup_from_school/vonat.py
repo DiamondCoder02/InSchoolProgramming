d1894196fbae009da8f62feb2ffca7603df3d529
Diamond <53056204+DiamondPRO02@users.noreply.github.com>
2022-01-27 15:31:07 +0100

    I like trains2

--- Backup_from_school/vonat.py
+++ Backup_from_school/vonat.py
@@ -17,7 +17,7 @@ print(menetrend)
 print("\n2.feladat")
 print("Az állomások száma:", len(menetrend[1]))
 print("A vonatok száma:", len(menetrend))
-print("3.feladat")
+print("\n3.feladat")
 legtobb={"állásidő":menetrend[1][1]["Indulás"]-menetrend[1][1]["Érkezés"],"vonat":1,"állomás":1}
 for vonat in menetrend:
     for állomás in range(1,len(menetrend[vonat])-1):
@@ -26,3 +26,32 @@ for vonat in menetrend:
             legtobb["vonat"]=vonat
             legtobb["állomás"]=állomás
 print("A(z)", legtobb["vonat"],"vonat a(z)",legtobb["állomás"],"állomáson", legtobb["állásidő"],"percet ált")
+print("\n4.feladat")
+bevonat=int(input("Adjon meg egy vonat azonosítóját!"))
+oraperc=input("Adjon meg egy időpontot (óra perc) !")
+print("\n5.feladat")
+eloirt=2*60+22
+elteres=eloirt-(menetrend[bevonat][len(menetrend[bevonat])-1]["Érkezés"]-menetrend[bevonat][0]["Indulás"])
+if elteres<0:
+    print("A(z) {}.vonat útja".format(bevonat), abs(elteres), "perccel rövidebb volt az előírtnál")
+elif elteres==0:
+    print("A(z)", bevonat, ". vonat utja pontosan az előírt ideig tartott.")
+else:
+    print("A(z)",bevonat,". vonat útja ", abs(elteres), " perccel hosszabb volt az előírtnál")
+#6.feladat
+def valto(x):
+    return str(x//60)+":"+str(x%60)
+fajlnev="halad"+str(bevonat)+".txt"
+with open(fajlnev,"w",encoding="utf-8") as halad:
+    for állomás in range(1, len(menetrend[bevonat])):
+        print(állomás,". állomás:", valto(menetrend[bevonat][állomás]["Érkezés"]), file=halad)
+print("7. feladat")
+idopont=int(oraperc.split()[0])*60+int(oraperc.split()[1])
+for vonat in menetrend:
+    if menetrend[vonat][0]["Indulás"]<menetrend[vonat][1]["Érkezés"]:
+        print("A(z)", vonat, ". vonat a 0. és az 1. állomás között járt.")
+    for állomás in range(1, len(menetrend[vonat])-1):
+        if menetrend[vonat][állomás]["Érkezés"]<=idopont<=menetrend[vonat][állomás]["Indulás"]:
+            print("A(z)", vonat, "vonat a",állomás,". állomáson állt")
+        if menetrend[vonat][állomás]["Indulás"]<idopont<menetrend[vonat][állomás+1]["Érkezés"]:
+            print("A(z)", vonat, "vonat a",állomás,"és a ",állomás+1,". állomás között járt")
